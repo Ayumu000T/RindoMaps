fc4254753424a4f2a94a435fbeb2037f95e74b4d
Ayumu <ic21at@gmail.com>
2024-07-27 17:06:50 +0900

    URLにloading=asyncを追加
    apiの警告がこれで消えた

--- resources/js/class/MapManager.js
+++ resources/js/class/MapManager.js
@@ -21,7 +21,7 @@ export class MapManager {
                 return;
             }
             const script = document.createElement('script');
-            script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+            script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&loading=async&callback=initMap`;
             script.async = true;
             script.defer = true;
             script.onerror = reject;
