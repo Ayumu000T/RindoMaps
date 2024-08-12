72346b04ce2648871a32cd33de715d323e69a125
Ayumu <ic21at@gmail.com>
2024-08-12 14:38:22 +0900

    レスポンシブ対応のレイアウト
    -bootstrapのmdサイズ以下で表示を変更
    -ひとまず仮のデザインなので、今後アップデートしていく

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,33 +1,38 @@
 
 <x-layout>
+
     <div class="container-fluid p-0 d-flex flex-column min-vh-100">
-        <header class="px-4 py-3">
+        <header class="px-4 py-3 d-flex justify-content-between">
             <h1 id="title" class="m-0 p-0">
                 <a href="{{ route('index') }}">
-                    <img class="" src="storage/header/rindo_map_logo.png" alt="" height="60">
+                    <img class="d-none d-md-block" src="storage/header/rindo_map_logo.png" alt="" height="60">
+                    <img class="d-md-none" src="storage/header/rindo_map_logo_mini.png" alt="" height="40">
                 </a>
             </h1>
             <menu id="menu" class="my-0">
-                <h3 id="about_rindo" class="m-0 p-0">
+                <h3 id="about_rindo" class="m-0 p-0 d-none d-md-block">
                     <img src="storage/header/rindo_icon.png" class="" alt="" width="40">
                 </h3>
-                <h3 id="about_map" class="m-0 p-0">
+                <h3 id="about_map" class="m-0 p-0 d-none d-md-block">
                     <img src="storage/header/map_icon.png" alt="" width="40">
                 </h3>
-                <h3 id="lets_go" class="m-0 p-0">
+                <h3 id="lets_go" class="m-0 p-0 d-none d-md-block">
                     <img src="/storage/header/helmet_icon.png" alt="" width="40">
                 </h3>
+                <i class="bi bi-three-dots-vertical d-md-none"></i>
             </menu>
         </header>
 
-        <div class="maps d-flex flex-grow-1">
-            <div class="rindo col-2">
-                {{-- 難易度を選択 --}}
+        <div class="d-flex flex-column-reverse flex-md-row flex-grow-1">
+            <div id="map" class="col-12 col-md-10 order-1 order-md-1">
+            </div>
+
+            <div class="rindo col-12 col-md-2 order-0 order-md-0">
                 <form class="mt-2 ms-3" id="difficulty_form" method="POST">
                     @csrf
                     <div class="d-flex">
                         <h5 class="col-4">難易度</h5>
-                        <select class="col-4" name="input_difficulty" id="difficulty_select" >
+                        <select class="col-4" name="input_difficulty" id="difficulty_select">
                             <option value="selectAllDifficulty">指定無し</option>
                             @foreach ($allDifficulties as $difficultyOption)
                                 <option value="{{ $difficultyOption->id }}">
@@ -37,7 +42,6 @@
                         </select>
                     </div>
                 </form>
-                {{-- 都道府県を選択 --}}
                 <form class="mt-1 ms-3" method="POST">
                     @csrf
                     <div class="d-flex">
@@ -52,7 +56,6 @@
                         </select>
                     </div>
                 </form>
-                {{-- 林道のリスト --}}
                 <div id="result" class="mt-2 mx-3">
                     <ul id="result_list">
                         @foreach ($spots as $spot)
@@ -60,16 +63,16 @@
                                 data-id="{{ $spot->id }}"
                                 data-coordinates="{{ $spot->coordinates }}"
                                 data-difficulty="{{ $spot->difficulty_display }}"
-                                data-image-url="{{  $spot->image_url }}">
+                                data-image-url="{{ $spot->image_url }}">
                                 {{ $spot->name }}
                             </li>
                         @endforeach
                     </ul>
                 </div>
             </div>
-            <div id="map" class="col-10"></div>
         </div>
     </div>
+
 
     {{-- 詳細とヘッダーメニューの表示 --}}
     <div id="detail_container">
